67dd88e679bff59f485996f8390ea33991e0fc7c
rafiramdani321 <rafiramdani321@gmail.com>
2024-09-21 22:46:39 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# whack-a-mole
+Whack A Mole Game
+https://rafiramdani1.github.io/whack-a-mole/
---
+++ js/script.js
@@ -0,0 +1,54 @@
+const tanah = document.querySelectorAll('.tanah')
+const tikus = document.querySelectorAll('.tikus')
+const papanSkor = document.querySelector('.papanSkor')
+
+let tanahSebelumnya
+let selesai
+let skor
+
+function randomTanah(tanah) {
+    const t = Math.floor(Math.random() * tanah.length)
+    const tRandom = tanah[t]
+    if (tRandom == tanahSebelumnya) {
+        randomTanah(tanah)
+    }
+    tanahSebelumnya = tRandom
+    return tRandom
+}
+
+function randomWaktu(min, max) {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+function munculkanTikus() {
+    const tRandom = randomTanah(tanah)
+    const rWaktu = randomWaktu(500, 1000)
+    tRandom.classList.add('muncul')
+    setTimeout(() => {
+        tRandom.classList.remove('muncul')
+        if (!selesai) {
+            munculkanTikus()
+        }
+    }, rWaktu)
+}
+
+function mulai() {
+    selesai = false
+    skor = 0
+    papanSkor.textContent = 0
+    munculkanTikus()
+    setTimeout(() => {
+        selesai = true
+    }, 10000)
+}
+
+function pukul() {
+    this.style.transition = "TOP 0.1s"
+    skor++
+    this.parentNode.classList.remove('muncul')
+    papanSkor.textContent = skor
+}
+
+tikus.forEach(t => {
+    t.addEventListener('click', pukul)
+})
